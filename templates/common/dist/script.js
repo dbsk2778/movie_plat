3a9ab8931a2de02a75633212d15bab2efb3a9714
Dogyeom Lee <glxm15@naver.com>
2022-10-25 09:38:04 +0900

    third commit

--- templates/common/dist/script.js
+++ templates/common/dist/script.js
@@ -1,3 +1,4 @@
+// 아이디, 비밀번호 입력 후 넘어가는 next button 관련
 $('.nextBtn').on('click',function(){
   var parentRef =  $(this).parent();
   var inputVal = parentRef.find('input').val();
@@ -14,7 +15,7 @@ $('.nextBtn').on('click',function(){
    parentRef.addClass('hide'); 
    parentRef.next().addClass('shown').addClass('visible');
    $('.bullets span.active').removeClass('active').next().addClass('active');
-  } else {
+  } else {  // 이메일,비밀번호 입력 후 성공시 진행되는 페이지 관련
     $('.container').css('background','#0C0C0C').html($('.spinnerWrapper').html());
     setTimeout(function(){
       $('.container').html('<div class="success"><i class="fa fa-check"></i><h2>Account Created</h2></div>');
@@ -23,6 +24,7 @@ $('.nextBtn').on('click',function(){
   $('.container').removeClass('error');
 });
 
+// 이메일, 비밀번호 체크 관련
 function checkLength(data,length){
   data = $.trim(data);
   if(data.length < length){
@@ -37,3 +39,4 @@ var add_remove_effects = function(ref,classname){
       $a.removeClass($b);
     },450);     
 }
+
