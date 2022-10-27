bb461994fedda18d12117d3dcebb4b75e501ed91
Areum Choi <pipi0_0@naver.com>
2022-10-27 10:24:13 +0900

    1026 수정..

--- user/urls.py
+++ user/urls.py
@@ -7,8 +7,9 @@ app_name = 'user'
 urlpatterns = [
     # 회원가입 페이지로 이동하거나 회원가입 처리를 하는 뷰(아름1025)
     path('signup/', views.signup, name='signup'),
-
+    # 로그인 처리 뷰.
+    path('login/', views.login, name='login'),
     # 로그아웃 처리를 하는 뷰.
     # next_page: 로그아웃 처리 후 리다이렉트할 URL 값. 기본 값은 None(아름1025)
-    # path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,18 +1,34 @@
+
 from django.contrib import auth
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-# Create your views here.
-# 회원 가입처리 (아름1025)
+
+# 회원가입 (아름1025)
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
-                                            username=request.POST['username'],
-                                            password=request.POST['password1'],
-                                            email=request.POST['email'],)
+                                            username = request.POST['username'],
+                                            email = request.POST['email'],
+                                            password = request.POST['password1'],
+                                            )
             auth.login(request, user)
             return redirect('/')
-        return render(request, 'user/signup.html')
-    return render(request, 'common/home.html')
+    return render(request, 'user/signup.html')
+
+# 로그인 (아름)
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email = email, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'user/login.html', {'error' : 'email or passeord is incorrect.'})
+    else:
+        return render(request, 'user/login.html')
+
