e8141b5ccb416753a6d14c69e9e9006b53ddc8fa
Areum Choi <pipi0_0@naver.com>
2022-10-31 14:05:14 +0900

    1031 pm14:05 save

--- user/views.py
+++ user/views.py
@@ -1,34 +1,17 @@
+from audioop import reverse
+from django.contrib.auth import models, forms
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from django.shortcuts import render
 
-from django.contrib import auth
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
 
-
-# 회원가입 (아름1025)
-def signup(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                                            username = request.POST['username'],
-                                            email = request.POST['email'],
-                                            password = request.POST['password1'],
-                                            )
-            auth.login(request, user)
-            return redirect('/')
-    return render(request, 'user/signup.html')
-
-# 로그인 (아름)
-def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(request, email = email, password = password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'user/login.html', {'error' : 'email or passeord is incorrect.'})
-    else:
-        return render(request, 'user/login.html')
-
+class UserCreateView(generic.CreateView):
+    """
+    내장 뷰 클래스인 CreateView를 상속하여 구현한 사용자 정의 CreateView.
+    GET 요청 시 회원가입 페이지로 이동하고,
+    POST 요청 시 내장 모델 클래스인 User를 사용하여 회원가입 처리를 한다.
+    """
+    # form_class 변수를 통해 일괄 설정하는 경우.
+    form_class = forms.UserCreationForm
+    template_name = 'user/signup.html'  # GET 요청을 처리할 때 응답할 템플릿 파일.
+    success_url = reverse_lazy('user:login')  # POST 요청을 처리할 때 리다이렉트할 URL.
