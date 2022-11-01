73f4241b8d08f7b7fd7f27996a76d2c0fbdb6561
Areum Choi <pipi0_0@naver.com>
2022-11-01 13:26:55 +0900

    signup-select삭제..

--- user/views.py
+++ user/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 
-
 class UserCreateView(generic.CreateView):
     """
     내장 뷰 클래스인 CreateView를 상속하여 구현한 사용자 정의 CreateView.
@@ -15,3 +14,4 @@ class UserCreateView(generic.CreateView):
     form_class = forms.UserCreationForm
     template_name = 'user/signup.html'  # GET 요청을 처리할 때 응답할 템플릿 파일.
     success_url = reverse_lazy('user:login')  # POST 요청을 처리할 때 리다이렉트할 URL.
+
